75206470279f4bd08605718ba4c882efd5216c9b
Dax Chen <daxchen@google.com>
2020-09-18 15:47:47 -0700

    mac enable key repeat

--- setup-macOS.sh
+++ setup-macOS.sh
@@ -15,9 +15,11 @@
 defaults write com.apple.dock autohide-time-modifier -float 0.1
 # make Dock come out with out delay
 defaults write com.apple.dock autohide-delay -float 0
-
 # restart Dock
 killall Dock
+
+# enable key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 
 ################
